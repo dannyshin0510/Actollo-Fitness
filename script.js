5aca251b5982f37aae190ab71730e8c47766e8fb
dannyshin0510 <shindannybms@gmail.com>
2019-01-02 02:27:24 -0500

    more efficient algorithm to output regiment

--- script.js
+++ script.js
@@ -20,66 +20,109 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Toronto,ca&units=me
 
 		//console.log(dayName[dateToday.getDay()]);
 		displayToday();
-		sunday();
-
+		
+		var mondayWork= ["Russian Twists: x30", "Leg raises x30", "Plank 2mins", "Mountain Climber 1min", "Squats x20"];
+		var tuesdayWork= ["Dumbell Flies x20", "Barbell Shoulder Raises x30", "Barbell Rows x20"];
+		var wednesdayWork= ["Congrats, Athlete! It's REST Day!"];
+		var thursdayWork= ["Pistol Squats x40","Wide Lunges x20", "Vertical Jumps x10", "Squats (Heavy) x10"];
+		var fridayWork= ["Pull Ups x20", "Chin holds 1min", "Neutral Hangs 2mins", "Muscle Ups x10"];
+		var saturdayWork= ["Pushups x40", "Crunches: x30", "Starjumps x30", "Leg Raises x30", "Jumprope x200"];
+		var sundayWork= ["Congrats, Athlete! It's REST day!"];
 		function displayToday()
 		{
 			document.getElementById("dayWeek").innerHTML="Today is: "+dayName[dateToday.getDay()];
 		}
+		if (dayName[dateToday.getDay()]=="Monday")
+			monday();
+		else if (dayName[dateToday.getDay()]=="Tuesday")
+			tuesday();
+		else if (dayName[dateToday.getDay()]=="Wednesday")
+			wednesday();
+		else if (dayName[dateToday.getDay()]=="Thursday")
+			thursday();
+		else if (dayName[dateToday.getDay()]=="Friday")
+			friday();
+		else if (dayName[dateToday.getDay()]=="Saturday")
+			saturday();
+		else 
+			sunday();
 		function monday ()
 		{
+			
 			if (temperature<10)
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
+				for (var i=0; i<mondayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+mondayWork[i]+"</li>";
+				}
+				
 			else
 				document.getElementById("work").innerHTML="Let's go for a run!";
 		}
 		function tuesday ()
 		{
 			if (temperature<10)
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
+				for (var i=0; i<tuesdayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+tuesdayWork[i]+"</li>";
+				}
+				
 			else
 				document.getElementById("work").innerHTML="Let's go for a run!";
 		}
 		function wednesday ()
 		{
 			if (temperature<10)
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
+				for (var i=0; i<wednesdayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+wednesdayWork[i]+"</li>";
+				}
+				
 			else
 				document.getElementById("work").innerHTML="Let's go for a run!";
 		}
 		function thursday ()
 		{
 			if (temperature<10)
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
+				for (var i=0; i<thursdayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+thursdayWork[i]+"</li>";
+				}
+				
 			else
 				document.getElementById("work").innerHTML="Let's go for a run!";
 		}
 		function friday ()
 		{
 			if (temperature<10)
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
+				for (var i=0; i<fridayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+fridayWork[i]+"</li>";
+				}
+				
 			else
 				document.getElementById("work").innerHTML="Let's go for a run!";
 		}
 		function saturday ()
 		{
 			if (temperature<10)
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
+				for (var i=0; i<saturdayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+saturdayWork[i]+"</li>";
+				}
+				
 			else
 				document.getElementById("work").innerHTML="Let's go for a run!";
 		}
 		function sunday ()
 		{
 			if (temperature<10)
-			{
-				document.getElementById("location").innerHTML="Today's location: Gym";
-				document.getElementById("work").innerHTML="russian twists, jump squats, pistol squats";
-			}
+				for (var i=0; i<sundayWork.length;i++)
+				{
+					document.getElementById("work").innerHTML+= "<li>"+sundayWork[i]+"</li>";
+				}
+				
 			else
-			{
-				document.getElementById("location").innerHTML="Today's location: Outside";
 				document.getElementById("work").innerHTML="Let's go for a run!";
-			}
 		}
 	}
 ); 
